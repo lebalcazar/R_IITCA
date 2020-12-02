a10e00f8b1074949b772ea5ee805ba1accc49180
lebalcazar <lebalcazar@gmail.com>
2020-12-02 15:48:15 -0600

    Se genera el scrip para leer datos de los paquetes de R

---
+++ R/02_ caragar datasets de R.R
@@ -0,0 +1,17 @@
+
+# Cargar "data sets" en R -------------------------------------------------
+
+# Los paquetes de R tienen una base datos que se pueden utilizar 
+
+# También se puede cargar la librería datasets 
+library(datasets)
+
+
+# conjunto de datos del género iris
+iris
+
+# conjuntos de datos marcas de vehículos
+mtcars
+
+# datos 
+datasets::CO2
