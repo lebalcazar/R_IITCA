335fe89adab8b849558bacdd18c5eb8c678d6ce2
lebalcazar <lebalcazar@gmail.com>
2020-12-09 17:38:32 -0600

    cambios menores

--- R/04_ggplot2.R
+++ R/04_ggplot2.R
@@ -28,54 +28,47 @@ plot <- plot(Sepal.Length ~ Petal.Length, data = iris,
 # https://ggplot2.tidyverse.org/ 
 
 
-iris %>% 
-  ggplot(aes(x = Sepal.Length, y = Petal.Length)) +
-  geom_point(colour = 'blue')
+  ggplot(iris, mapping = aes(x = Sepal.Length, y = Petal.Length)) + 
+  geom_point(colour = 'blue') +
+  theme_bw()
 
+# color por especie
 iris %>% 
   ggplot(aes(x = Sepal.Length, y = Petal.Length, colour = Species)) +
   geom_point() +
   theme_bw() +
-  labs(x = 'Longitud del Sépalo', y = 'Longitud del Pétalo', colour = 'Especies')
+  labs(x = 'Longitud del Sépalo', 
+       y = 'Longitud del Pétalo', colour = 'Especies')
 
 iris %>% 
   ggplot(aes(x = Sepal.Length, y = Petal.Length, color = Species)) +
   geom_point() +
   stat_smooth(method = 'lm', se = F) +
- 
-
-  #scale_color_ordinal(guide = guide_legend()) 
-  guides(color = guide_legend(
-    title = 'Especies',
-    title.theme = element_text(vjust = 0.5,
-                               size = 10,
-                               face = "bold",
-                               colour = "black"))) +
- 
+  labs(x = 'Longitud del Sépalo', y = 'Longitud del Pétalo') +
   theme_bw() +
   theme(legend.position = c(0.85, 0.2),
-        panel.grid.minor = element_blank()) +
+        panel.grid.minor = element_blank()) 
  
-  labs(x = 'Longitud del Sépalo', y = 'Longitud del Pétalo')
 
 
 
-# histograma con la lluvia media mensual
+# graficar la lluvia media mensual ----------------------------------
 
 # cargamos la precipitación mensual obtenida en el programa anterior
 load('resultados/tablas/prc.mes.rds')
 
-    # aquí empieza ggplot2. Toma los datos de prc.mes como entrada
+# aquí empieza ggplot2. Toma los datos de prc.mes como entrada
 ggplot(data = prc.mes) +
   # para que no se apilen las columnas utilizamos posición 'dodge'
   geom_col(mapping = aes(x = mes, 
-                         y = prcMes, 
-                         fill = nombre),
-           position = 'dodge') +  
+                               y = prcMes, 
+                               fill = nombre),
+                 position = 'dodge') +  
   # se modifican las escalas "x", "y", y "fill", relleno de las columnas  
   scale_x_continuous(name = '',
                      breaks = 1:12,
                      labels = month(1:12, label = T)) +
+  # modificar el nombre del eje y
   scale_y_continuous(name = expression(paste('Preciptación (mm',' ', mes^-1, ')'))
   ) +
   scale_fill_manual(name = 'Estaciones', 
@@ -95,72 +88,51 @@ ggsave('resultados/plots/prc.mes.png')
          dpi = 600, width = 4, height = 3)
 
 
+
+  # utilizando facets  
+  ggplot(data = prc.mes) +
+        geom_col(mapping = aes(x = mes, 
+                           y = prcMes, 
+                           fill = nombre),
+             position = 'dodge') +  
+   
+    scale_x_continuous(name = '',
+                       breaks = 1:12,
+                       labels = month(1:12, label = T)) +
+  
+    scale_y_continuous(name = expression(paste('Preciptación (mm',' ', mes^-1, ')'))
+    ) +
+    scale_fill_manual(name = 'Estaciones', 
+                      values = c('black', 'grey20', 'grey40', 'grey60')) +
+    facet_wrap(~nombre) +
+    
+  theme_bw() +
+    theme(legend.position = c(0.10, 0.80),
+          panel.grid = element_blank()
+    )
+  
+  ggsave(filename = 'prc.mes_facets.png', plot = last_plot(), 
+         path = 'resultados/plots/', device = 'png',
+         dpi = 600, width = 7, height = 5)
+  
+
 # trabajaremos con los datos millas
-millas <- millas
+millas <- tibble(millas)
+
 millas %>% 
   # cilindrada = tamaño del motor (litros)
   # autopista = eficiencia del vehículo (millas por galón en autopista)
-  ggplot(aes(x = cilindrada, y = autopista)
-  ) +
+  ggplot(aes(x = cilindrada, y = autopista)) +
   geom_point()
 
 
 millas %>% 
-# agregamos uan tercera vsariable
-  ggplot(aes(x = cilindrada, y = autopista, colour = clase)
-  ) +
+# agregamos uan tercera variable
+  ggplot(aes(x = cilindrada, y = autopista, colour = clase)) +
   geom_point()
   
   
-  
-df2 <- data.frame(sex = rep(c("Female", "Male"), each=3),
-                  time=c("breakfeast", "Lunch", "Dinner"),
-                  bill=c(10, 30, 15, 13, 40, 17) )
 
-ggplot(df2, aes(x=time, y=bill, group=sex)) +
-  geom_line(aes(linetype=sex))+
-  geom_point()+
-  scale_linetype_manual(values=c("twodash", "dotted"))+
-  theme(legend.position="top")  
-
-# Line plot with multiple groups
-ggplot(data=df2, aes(x=time, y=bill, group=sex)) +
-  geom_line()+
-  geom_point()
-# Change line types
-ggplot(data=df2, aes(x=time, y=bill, group=sex)) +
-  geom_line(linetype="dashed")+
-  geom_point()
-
-# Change line colors and sizes
-ggplot(data=df2, aes(x=time, y=bill, group=sex)) +
-  geom_line(linetype="dotted", color="red", size=2)+
-  geom_point(color="blue", size=3)
-
-ggplot(df2, aes(x=time, y=bill, group=sex)) +
-  geom_line(aes(linetype=sex))+
-  geom_point()+
-  theme(legend.position="top")
-# Change line types + colors
-ggplot(df2, aes(x=time, y=bill, group=sex)) +
-  geom_line(aes(linetype=sex, color=sex))+
-  geom_point(aes(color=sex))+
-  theme(legend.position="top")
-
-# Set line types manually
-ggplot(df2, aes(x=time, y=bill, group=sex)) +
-  geom_line(aes(linetype=sex))+
-  geom_point()+
-  scale_linetype_manual(values=c("twodash", "dotted"))+
-  theme(legend.position="top")
-# Change line colors and sizes
-ggplot(df2, aes(x=time, y=bill, group=sex)) +
-  geom_line(aes(linetype=sex, color=sex, size=sex))+
-  geom_point()+
-  scale_linetype_manual(values=c("twodash", "dotted"))+
-  scale_color_manual(values=c('#999999','#E69F00'))+
-  scale_size_manual(values=c(1, 1.5))+
-  theme(legend.position="top")
 
 
 
