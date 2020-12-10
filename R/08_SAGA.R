0ae2476995cc8f3582d5750e569442b84aec19b2
gavg712 <gavg712@gmail.com>
2020-12-10 10:49:41 -0500

    Agregado procedimiento para Cuenca aportante y otras sugerencias de SAGA en R

--- R/08_SAGA.R
+++ R/08_SAGA.R
@@ -10,11 +10,14 @@ library(tmap)      # pkg para desplegar objetos (raster, poligonos, etc)
 env <- rsaga.env(path = 'C:/Program Files (x86)/SAGA-GIS')
 env
 
-# desplegar las librerias de SAGA
+# Consultar los nombres de las librerias de SAGA
 rsaga.get.libraries()
 
-# modulos del la librería preprocessor 
+# Consultar los módulos de la librería preprocessor 
 rsaga.get.modules("ta_preprocessor")
+
+# Consultar como se usa un módulo: Se puede usar el nombre o el número de módulo
+rsaga.get.usage("ta_preprocessor", module = "Fill Sinks (Wang & Liu)")
 
 # función para rellenar depresiones del terreno
 rsaga.fill.sinks(in.dem = "datos/raster/demEscondido/s_b1w24290utm.rst",
@@ -41,7 +44,7 @@ raster('resultados/raster/hillshade.sdat') %>% plot()
 rsaga.get.modules("ta_hydrology")
 rsaga.get.modules("ta_channels")
 
-
+#···································
 # acumulación de flujo
 rsaga.topdown.processing(in.dem = "resultados/raster/escondido/alt_fill.sgrd", 
                          out.carea = "resultados/raster/escondido/flujo",  
@@ -50,7 +53,7 @@ rsaga.topdown.processing(in.dem = "resultados/raster/escondido/alt_fill.sgrd",
 flujo <- raster("resultados/raster/escondido/flujo.sdat")
 spplot(flujo)
 
-
+#···································
 # redes de cauces 
 rsaga.get.modules("ta_channels", env = env)
 rsaga.get.usage("ta_channels", "Channel Network",env = env)
@@ -67,15 +70,15 @@ cauces <- st_read("datos/raster/puntoEscondido/channel_net_shp.shp")
 
 spplot(as(cauces, "Spatial"), "Order")
 
-
+#···································
 # delimitación de cuencas
 rsaga.get.usage("ta_channels","Watershed Basins",env = env)
 
 
 rsaga.geoprocessor("ta_channels",1,
                    list(ELEVATION = "resultados/raster/escondido/alt_fill.sgrd", 
-                        CHANNELS = "resultados/raster/escondido/channel_net.sgrd", 
-                        BASINS = "resultados/raster/escondido/basins.sgrd",
+                        CHANNELS = "resultados/raster/escondido/channel_net", 
+                        BASINS = "resultados/raster/escondido/basins",
                         MINSIZE = 500), 
                    env = env)
 
@@ -84,9 +87,41 @@ cuencaEscondido <- raster("resultados/raster/escondido/basins.sdat")
 spplot(cuencaEscondido)
 
 
+#···································
+# Cuenca aportante a un punto de salida.
+rsaga.get.modules("ta_hydrology", env = env)
+rsaga.get.usage(lib = "ta_hydrology", module = 4, env = env)
 
+# Coordenada del punto de salida en el mismo SRC del DEM
+pxy <-  c(x = 347724.5180, y = 3172076,5631)
 
+# Calculemos la cuenca aportante a ese punto
+rsaga.geoprocessor(
+  lib = "ta_hydrology", 
+  module = 4,
+  param = list(TARGET_PT_X = pxy[1], 
+               TARGET_PT_Y = pxy[2], 
+               ELEVATION = "resultados/raster/escondido/alt_fill.sgrd",
+               METHOD = 2, #Método MFD de acuerdo a #[1]
+               AREA = "resultados/raster/escondido/cuenca_aporte.sgrd"),
+  env = env
+)
 
+cuencaAportante <- raster("resultados/raster/escondido/cuenca_aporte.sdat")
 
+# afinar los valores mediante reclasificación
+cuencaAportante[cuencaAportante <= 0] <- NA
+cuencaAportante[cuencaAportante > 0] <- 1
 
+# Plot del DEM
+raster("resultados/raster/escondido/alt_fill.sdat") %>% 
+  plot()
 
+# Agregar cuenca aportante
+plot(cuencaAportante, add = TRUE, legend = FALSE)
+
+# agregar el punto de salida
+points(pxy[1], pxy[2], cex = 1, pch = 16)
+
+# Referencias ----
+#[1] https://www.sciencedirect.com/science/article/pii/009830049190048I
